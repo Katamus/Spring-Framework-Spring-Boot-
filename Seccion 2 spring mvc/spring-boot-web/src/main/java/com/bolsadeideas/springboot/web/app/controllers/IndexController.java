68c5b2c3f75b4a5e927fbe857631998150f728d8
Christian <christiancatamuscay@r-fast.com>
2022-12-10 17:05:56 -0500

    15. El objeto Model

--- Seccion 2 spring mvc/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ Seccion 2 spring mvc/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 
 @Controller
 @RequestMapping("/app")
@@ -13,6 +16,16 @@ public class IndexController {
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola Spring Framework con model!");
 		return "index";
+	}
+	
+	@RequestMapping(value = "/perfil" , method = RequestMethod.GET )
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Andrés");
+		usuario.setApellido("Guzmán");
+		model.addAttribute("usuario",usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		return "perfil";
 	}
 
 }
