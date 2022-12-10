5c3139440fb52e83f9f784f27202cef4a714b002
Christian <christiancatamuscay@r-fast.com>
2022-12-10 17:41:09 -0500

    18. Anotacion @ModelAttribute

--- Seccion 2 spring mvc/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ Seccion 2 spring mvc/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -35,15 +36,18 @@ public class IndexController {
 	
 	@RequestMapping(value = "/listar" , method = RequestMethod.GET )
 	public String listar(Model model) {
-		List<Usuario> usuarios = Arrays.asList(
-					new Usuario("Andres","Guzman","andres@correo.com"),
-					new Usuario("Jhon","Guzman","andres@correo.com"),
-					new Usuario("Jay","Guzman","andres@correo.com"),
-					new Usuario("Tornado","Roe","andres@correo.com"));
-		
 		model.addAttribute("titulo", "Listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
 		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+		List<Usuario> usuarios = Arrays.asList(
+				new Usuario("Andres","Guzman","andres@correo.com"),
+				new Usuario("Jhon","Guzman","andres@correo.com"),
+				new Usuario("Jay","Guzman","andres@correo.com"),
+				new Usuario("Tornado","Roe","andres@correo.com"));
+		return usuarios;
 	}
 
 }
