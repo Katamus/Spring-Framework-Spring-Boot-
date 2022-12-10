c228bfefe5ded6ca3dadfd6c9b4fabd8050b22e8
Christian <christiancatamuscay@r-fast.com>
2022-12-10 16:43:44 -0500

    13. Anotacion @RequestMapping sobre el controllador

--- Seccion 2 spring mvc/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ Seccion 2 spring mvc/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,13 +3,15 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/index","/","/home"})
+	@GetMapping({"/index","","/","/home"})
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring Framework con model and view!");
+		model.addAttribute("titulo", "Hola Spring Framework con model!");
 		return "index";
 	}
 
