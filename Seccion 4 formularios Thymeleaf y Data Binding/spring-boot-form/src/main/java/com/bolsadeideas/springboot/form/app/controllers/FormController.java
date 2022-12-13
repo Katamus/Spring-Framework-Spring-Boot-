c5d44f64c53d9214c8d83dc8de66192aefb466dd
Christian <christiancatamuscay@r-fast.com>
2022-12-12 21:25:14 -0500

    54. Añadiendo los mensajes de errores en la vista del formulario

--- Seccion 4 formularios Thymeleaf y Data Binding/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ Seccion 4 formularios Thymeleaf y Data Binding/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -21,6 +21,7 @@ public class FormController {
 	@GetMapping("/form")
 	public String form(Model model) {
 		model.addAttribute("titulo","Formulario usuario");
+		model.addAttribute("usuario",new Usuario());
 		return "form";
 	}
 	
@@ -31,7 +32,7 @@ public class FormController {
 		if(result.hasErrors()) {
 			Map<String, String> errores = new HashMap<>();
 			result.getFieldErrors().forEach(err->{
-				errores.put(err.getField(), "El campo".concat(err.getField()).concat(err.getDefaultMessage()));
+				errores.put(err.getField(), "El campo ".concat(err.getField()+" ").concat(err.getDefaultMessage()));
 			});
 			model.addAttribute("error", errores);
 			return "form";
