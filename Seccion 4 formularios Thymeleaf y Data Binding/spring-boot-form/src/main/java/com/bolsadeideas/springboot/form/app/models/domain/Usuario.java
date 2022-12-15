f8a71fbf8b9bb7e5dc03cbfc9ed7d1a7571426e1
Christian <christiancatamuscay@r-fast.com>
2022-12-14 19:49:28 -0500

    66. Validación de números enteros con @Max @Min y NotNull, Uso de la custumizacion de un mensaje por error de convercion del tipos tanto uso global como directo a una propiedad de una clase

--- Seccion 4 formularios Thymeleaf y Data Binding/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ Seccion 4 formularios Thymeleaf y Data Binding/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,7 +1,10 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.bolsadeideas.springboot.form.app.validation.IdentificadorRegex;
@@ -28,6 +31,11 @@ public class Usuario {
 	
 	@Requerido
 	private String apellido;
+	
+	@NotNull
+	@Min(5)
+	@Max(5000)
+	private Integer cuenta;
 	
 	
 	public String getUsername() {
@@ -65,6 +73,12 @@ public class Usuario {
 	}
 	public void setIdentificador(String identificador) {
 		this.identificador = identificador;
+	}
+	public Integer getCuenta() {
+		return cuenta;
+	}
+	public void setCuenta(Integer cuenta) {
+		this.cuenta = cuenta;
 	}
 
 	
