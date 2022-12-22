99a8632a22c0a327d0df502b8d45cd75897f07f1
Christian <christiancatamuscay@r-fast.com>
2022-12-22 11:24:17 -0500

    108. Manejando excepción personalizada creando clases Model y Service (Manejo de errores)

---
+++ Seccion 7 Manejo de errores/spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/services/UsuarioServiceImpl.java
@@ -0,0 +1,49 @@
+package com.bolsadeideas.springboot.error.app.services;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bolsadeideas.springboot.error.app.models.domain.Usuario;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+	
+	
+	private List<Usuario> lista;
+	
+	
+	
+	
+	public UsuarioServiceImpl() {
+		lista = Arrays.asList(
+				new Usuario(1,"a1","b1"),
+				new Usuario(2,"a2","b2"),
+				new Usuario(3,"a3","b3"),
+				new Usuario(4,"a4","b4"),
+				new Usuario(5,"a5","b5"),
+				new Usuario(6,"a6","b6"),
+				new Usuario(7,"a7","b7"),
+				new Usuario(8,"a8","b8")
+				);
+	}
+
+	@Override
+	public List<Usuario> listar() {
+		return lista;
+	}
+
+	@Override
+	public Usuario obtenerPorId(Integer id) {
+		Usuario usuario = null;
+		for (Usuario item : lista) {
+			if(id.equals(item.getId())) {
+				usuario = item; 
+				break;
+			}
+		}
+		return usuario;
+	}
+
+}
