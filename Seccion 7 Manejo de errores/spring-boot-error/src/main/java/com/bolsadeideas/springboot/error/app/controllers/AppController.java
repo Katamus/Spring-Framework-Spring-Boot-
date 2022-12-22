8ae95f9215173c6b74238b520dc66d57015a212d
Christian <christiancatamuscay@r-fast.com>
2022-12-22 11:51:05 -0500

    110. Creando clase Exception personalizada. (Manejo de errores)

--- Seccion 7 Manejo de errores/spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
+++ Seccion 7 Manejo de errores/spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.bolsadeideas.springboot.error.app.error.UsuarioNoEncontradoException;
 import com.bolsadeideas.springboot.error.app.models.domain.Usuario;
 import com.bolsadeideas.springboot.error.app.services.UsuarioService;
 
@@ -25,6 +26,9 @@ public class AppController {
 	@GetMapping("/ver/{id}")
 	public String ver ( @PathVariable Integer id,Model model ) {
 		Usuario usuario = usuarioService.obtenerPorId(id);
+		if( usuario == null ) {
+			throw new UsuarioNoEncontradoException(id.toString());
+		}
 		model.addAttribute("usuario", usuario);
 		model.addAttribute("titulo","Detalle usuario: ".concat(usuario.getNombre()));
 		return "ver";
--- Seccion 7 Manejo de errores/spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/ErrorHandlerController.java
+++ Seccion 7 Manejo de errores/spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/ErrorHandlerController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.bolsadeideas.springboot.error.app.error.UsuarioNoEncontradoException;
+
 @ControllerAdvice
 public class ErrorHandlerController {
 	
@@ -25,7 +27,16 @@ public class ErrorHandlerController {
 		model.addAttribute("message",ex.getMessage());
 		model.addAttribute("status",HttpStatus.INTERNAL_SERVER_ERROR.value());
 		model.addAttribute("timestamp", new Date());
-		return "error/generica";
+		return "error/numero-formato";
+	}
+	
+	@ExceptionHandler(UsuarioNoEncontradoException.class)
+	public String usuarioNoEncontrado(UsuarioNoEncontradoException ex, Model model) {
+		model.addAttribute("error","Error: usuario no encontrado!");
+		model.addAttribute("message",ex.getMessage());
+		model.addAttribute("status",HttpStatus.INTERNAL_SERVER_ERROR.value());
+		model.addAttribute("timestamp", new Date());
+		return "error/usuario";
 	}
 
 }
