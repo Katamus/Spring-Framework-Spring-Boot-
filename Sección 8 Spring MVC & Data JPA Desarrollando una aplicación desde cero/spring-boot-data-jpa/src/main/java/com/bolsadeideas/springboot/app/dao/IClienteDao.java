f7324a1ee82f7600a5a67ffda860d13beff4eda4
Christian <christiancatamuscay@r-fast.com>
2022-12-22 14:00:37 -0500

    115. Creando la clase de Acceso a Datos o Dao con Entity manager(Spring MVC & Data JPA)

---
+++ Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/dao/IClienteDao.java
@@ -0,0 +1,11 @@
+package com.bolsadeideas.springboot.app.dao;
+
+import java.util.List;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+public interface IClienteDao {
+	
+	public List<Cliente> findAll();
+
+}
