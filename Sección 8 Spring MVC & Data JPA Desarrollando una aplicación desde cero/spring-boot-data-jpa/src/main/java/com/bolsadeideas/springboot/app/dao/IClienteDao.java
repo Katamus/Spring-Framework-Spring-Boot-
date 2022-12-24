54ffd5091875684fc5179a093ccd9e889527dc8f
Christian <christiancatamuscay@r-fast.com>
2022-12-24 14:38:19 -0500

    131. implementadno el dao con la interface CrudRepository(Spring MVC & Data JPA)

--- Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/dao/IClienteDao.java
+++ Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/dao/IClienteDao.java
@@ -1,17 +1,9 @@
 package com.bolsadeideas.springboot.app.dao;
 
-import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao {
-	
-	public List<Cliente> findAll();
-	
-	void save(Cliente cliente) ;
-	
-	public Cliente findOne(Long id);
-	
-	public void delete(Long id);
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
 
 }
