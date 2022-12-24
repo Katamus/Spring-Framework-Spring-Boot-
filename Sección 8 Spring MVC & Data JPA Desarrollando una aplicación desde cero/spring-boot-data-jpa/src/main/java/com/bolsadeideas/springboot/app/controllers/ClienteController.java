0ed4425082b2abe98c9caa1687461648fc0d5d6c
Christian <christiancatamuscay@r-fast.com>
2022-12-24 17:27:33 -0500

    139. Paginador desde cero - Usando PagingAndSortingRepository - Parte I(Spring MVC & Data JPA: Mejorando la aplicación)

--- Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -3,12 +3,16 @@ package com.bolsadeideas.springboot.app.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -24,9 +28,11 @@ public class ClienteController {
 	private IClienteService clienteService;
 	
 	@RequestMapping(value = {"/listar","","/"}, method = RequestMethod.GET)
-	public String listar(Model model) {
+	public String listar(@RequestParam(name="page",defaultValue = "0") int page,Model model) {
+		Pageable pageable = PageRequest.of(page, 4);
+		Page<Cliente> clientes = clienteService.findAll(pageable);
 		model.addAttribute("titulo", "Litado de clientes");
-		model.addAttribute("clientes", clienteService.findAll());
+		model.addAttribute("clientes", clientes);
 		return "listar";
 	}
 	
