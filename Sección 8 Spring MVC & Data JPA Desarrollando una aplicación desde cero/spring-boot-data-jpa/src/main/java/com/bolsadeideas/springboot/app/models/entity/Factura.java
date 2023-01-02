c5c7debeacb9ee15cfee0ec71be5957ba5eef188
Christian <christiancatamuscay@r-fast.com>
2023-01-02 18:41:47 -0500

    160. Asociaciones: ManyToOne Unidireccional - Clases Entity ItemFactura y Producto(Sección 11: Desarrollando un Sistema de Facturación)

--- Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
+++ Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.app.models.entity;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -111,5 +112,14 @@ public class Factura implements Serializable {
 
 	public void addItemFactura(ItemFactura item) {
 		this.items.add(item);
+	}
+	
+	public Double getTotal(){
+		Double total = 0.0;
+		int size = items.size();
+		for (int i = 0; i < size; i++) {
+			total += items.get(i).calcularImporte();
+		}
+		return total;
 	}
 }
