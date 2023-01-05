39994060651bf38ea423bbdda6d07fdbac893efb
Christian <christiancatamuscay@r-fast.com>
2023-01-05 13:45:54 -0500

    211. Creando las entidades necesarias Usuario y Role(Spring Security: Autenticación con JPA)

---
+++ Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Usuario.java
@@ -0,0 +1,89 @@
+package com.bolsadeideas.springboot.app.models.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class Usuario implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true, length = 30)
+	private String username;
+	
+	@Column(length = 250)
+	private String password;
+	
+	private Boolean enable;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name="user_id")
+	private List<Rol> roles;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public Boolean getEnable() {
+		return enable;
+	}
+
+
+	public void setEnable(Boolean enable) {
+		this.enable = enable;
+	}
+
+
+	public List<Rol> getRoles() {
+		return roles;
+	}
+
+
+	public void setRoles(List<Rol> roles) {
+		this.roles = roles;
+	}
+
+
+	private static final long serialVersionUID = 1L;
+
+}
