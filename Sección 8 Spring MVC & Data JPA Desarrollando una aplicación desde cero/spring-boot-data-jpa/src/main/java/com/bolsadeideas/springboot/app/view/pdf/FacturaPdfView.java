2804c7b0b632123ca9258deb76785fcba943c365
Christian <christiancatamuscay@r-fast.com>
2023-01-06 14:21:44 -0500

    230. Escribiendo el código de las líneas de la Factura en la vista PDF(View Technologies: Exportar a PDF)

--- Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
+++ Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.bolsadeideas.springboot.app.models.entity.Factura;
+import com.bolsadeideas.springboot.app.models.entity.ItemFactura;
 import com.lowagie.text.Document;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
@@ -35,8 +38,29 @@ public class FacturaPdfView extends AbstractPdfView{
 		table2.addCell("Descripción: "+ factura.getDescripcion());
 		table2.addCell("Fecha: "+ factura.getCreateAt());
 		
+		
+		PdfPTable table3 = new PdfPTable(4);
+		table3.addCell("Producto");
+		table3.addCell("Precio");
+		table3.addCell("Cantidad");
+		table3.addCell("Total");
+		
+		for(ItemFactura itemFactura : factura.getItems()) {
+			table3.addCell(itemFactura.getProducto().getNombre());
+			table3.addCell(itemFactura.getProducto().getPrecio().toString());
+			table3.addCell(itemFactura.getCantidad().toString());
+			table3.addCell(itemFactura.calcularImporte().toString());
+		}
+		
+		PdfPCell cell = new PdfPCell(new Phrase("Total: "));
+		cell.setColspan(3);
+		cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+		table3.addCell(cell);
+		table3.addCell(factura.getTotal().toString());
+		
 		document.add(table);
 		document.add(table2);
+		document.add(table3);
 	}
 
 }
