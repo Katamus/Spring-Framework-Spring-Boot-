5cffb9fc41b174c001f532d1418e0716913b3594
Christian <christiancatamuscay@r-fast.com>
2023-01-09 12:02:44 -0500

    242. Implementando la clase ClienteCsvView y Link en vista listar clientes(View Technologies: Exportar a CSV)

--- Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/view/csv/ClientesCsvView.java
+++ Sección 8 Spring MVC & Data JPA Desarrollando una aplicación desde cero/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/view/csv/ClientesCsvView.java
@@ -5,8 +5,14 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
 @Component("listar")
 public class ClientesCsvView extends AbstractView{
@@ -21,12 +27,25 @@ public class ClientesCsvView extends AbstractView{
 		return true;
 	}
 
+	
 	@Override
+	@SuppressWarnings("unchecked")
 	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		response.setHeader("Content-Disposition", "attachment; filename=\"clientes.csv\"");
 		response.setContentType(getContentType());
 		
+		Page<Cliente> clientes = (Page<Cliente>) model.get("clientes");
+		
+		ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+		
+		String[] header = {"id","nombre","apellido","email","createAt"};
+		beanWriter.writeHeader(header);
+		
+		for(Cliente cliente: clientes) {
+			beanWriter.write(cliente, header);
+		}
+		beanWriter.close();
 	}
 
 }
