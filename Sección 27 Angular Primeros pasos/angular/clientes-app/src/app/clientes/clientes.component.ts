19d76a61facdde0869c632c5ffdfbcb3ad5330a9
Christian <christiancatamuscay@r-fast.com>
2023-01-15 19:54:20 -0500

    306. Listando los objetos del tipo Cliente(Angular: Componente clientes)

--- Sección 27 Angular Primeros pasos/angular/clientes-app/src/app/clientes/clientes.component.ts
+++ Sección 27 Angular Primeros pasos/angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
+
+
+  clientes:Cliente[] = [
+    {id: 1,nombre : 'Andrés',apellido: 'Guzmán',createAt: '2023-01-21', email: 'profesor@bolsadeideas.com' },
+    {id: 2,nombre : 'Mr Jon',apellido: 'Doe',createAt: '2023-01-20', email: 'joe.doe@gmail.com' },
+    {id: 3,nombre : 'Linus',apellido: 'Torvalds',createAt: '2023-01-19', email: 'linus.torvalds@gmail.com'}
+  
+  ];
 
   constructor() { }
 
