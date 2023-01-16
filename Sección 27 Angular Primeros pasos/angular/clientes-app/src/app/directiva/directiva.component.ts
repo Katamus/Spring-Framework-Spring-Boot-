2ce2933366993270963e3edc97564e46868d9913
Christian <christiancatamuscay@r-fast.com>
2023-01-15 19:34:11 -0500

    304. Directiva estructural *ngIf(Angular: Primeros pasos)

--- Sección 27 Angular Primeros pasos/angular/clientes-app/src/app/directiva/directiva.component.ts
+++ Sección 27 Angular Primeros pasos/angular/clientes-app/src/app/directiva/directiva.component.ts
@@ -6,8 +6,14 @@ import { Component } from '@angular/core';
 })
 export class DirectivaComponent{
 
+  habilitar:boolean = true;
+
   listaCurso:string[] = [`TypeScript`,`JavaScript`,`Java SE`,'C#']
 
   constructor() { }
+
+  setHabilitar():void {
+    this.habilitar = !this.habilitar;
+  }
 
 }
