812d2013395b70b62f16756c8c4bda99a8bb64d4
Christian <christiancatamuscay@r-fast.com>
2023-01-17 11:21:38 -0500

    333. Instalar SweetAlert2 para enviar mensajes de alerta en el cliente(Angular: CRUD con Spring API Rest)

--- Sección 27 Angular Primeros pasos/angular/clientes-app/src/app/clientes/form.component.ts
+++ Sección 27 Angular Primeros pasos/angular/clientes-app/src/app/clientes/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-form',
@@ -20,7 +21,10 @@ export class FormComponent implements OnInit {
 
   public create(){
     this.clienteService.create(this.cliente).subscribe(
-      response=> this.router.navigate(['/clientes'])
+      response=> {
+        this.router.navigate(['/clientes'])
+        Swal.fire('Nuevo cliente',`Cliente ${this.cliente.nombre} creado con éxotp!`,'success');
+      }
     )
   } 
 
